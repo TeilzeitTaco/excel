c9262834c206179a9d0e7f0be934a271e398df63
TeilzeitTaco <42586657+TeilzeitTaco@users.noreply.github.com>
2020-01-20 18:12:30 +0100

    changed db

--- stuff/server/app/__init__.py
+++ stuff/server/app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
 
 app = Flask(__name__)
-app.config.from_json("config.json")
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 from app import routes, models
